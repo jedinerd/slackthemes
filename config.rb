8eb8e9ac3bdf4f6be1f636d3650265eb95a543fb
Michael Hogg <mh@enterprisealumni.com>
2018-07-02 15:10:03 -0700

    more stuff

--- config.rb
+++ config.rb
@@ -90,11 +90,11 @@ end
 activate :deploy do |deploy|
   deploy.method = :git
   # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.remote   = 'git@github.com:jedinerd/EnterpriseJungleSlackThemes.git' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end if ENV['ANALYTICS_ID']
+end
 
 helpers do
   def themes
